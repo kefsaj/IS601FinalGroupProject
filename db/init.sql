809647316b64c2d12473b87ad571ab00359ac461
Kefin Sajan <kefsaj@github.com>
2021-05-11 22:46:04 -0400

    Task:  bug fixes and other informations

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE userData;
 use userData;
 
@@ -5,12 +6,13 @@ CREATE TABLE IF NOT EXISTS userTable(
     `id` int AUTO_INCREMENT,
     `username` VARCHAR(100) CHARACTER SET utf8,
     `password` VARCHAR(100) CHARACTER SET utf8,
-    `email` VARCHAR(100) CHARACTER SET utf8,
-    `isemailed` BOOLEAN,
+    `firstname` VARCHAR(100) CHARACTER SET utf8,
+    `lastname` VARCHAR(100) CHARACTER SET utf8,
+    `isactivate` BOOLEAN,
+
     PRIMARY KEY (`id`)
 );
 
-INSERT INTO userTable (username,password,email,lastname,isemailed) VALUES
-    ('ks664@gmail.com','123456', 'kef', 'saj', '1',TRUE),
-    ('ksajan16@gmail.com','123456', 'kefin', 'sajan', '1',TRUE);
-
+INSERT INTO userTable (username,password,firstname,lastname,school,department,year,isactivate) VALUES
+    ('ks664@njit.edu','1234', 'Kefin', 'Sajan',TRUE),
+    ('ab344@njit.edu','0000', 'Adrianna', 'Brzezinska',TRUE);
